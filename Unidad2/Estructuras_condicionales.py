56fd1881ffe6bfd09b232623daa87d662a58acb6
sabrinagimenez98 <sabrinita9898@gmail.com>
2025-08-28 00:28:45 -0300

    Update Estructuras_condicionales.py

---
+++ Unidad2/Estructuras_condicionales.py
@@ -0,0 +1,17 @@
+#1) Escribir un programa que solicite la edad del usuario. Si el usuario es mayor de 18 años,
+#deberá mostrar un mensaje en pantalla que diga “Es mayor de edad”.
+edad=int(input("Ingrese su edad: "))
+if edad >= 18:
+    print("Es mayor de edad")
+else:
+    print("No es mayor de edad")
+
+#2) Escribir un programa que solicite su nota al usuario. Si la nota es mayor o igual a 6, deberá
+#mostrar por pantalla un mensaje que diga “Aprobado”; en caso contrario deberá mostrar el
+#mensaje “Desaprobado”.
+
+
+#3) Escribir un programa que permita ingresar solo números pares. Si el usuario ingresa un
+#número par, imprimir por en pantalla el mensaje "Ha ingresado un número par"; en caso
+#contrario, imprimir por pantalla "Por favor, ingrese un número par". Nota: investigar el uso del
+#operador de módulo en Python para evaluar si un número es par o impar
