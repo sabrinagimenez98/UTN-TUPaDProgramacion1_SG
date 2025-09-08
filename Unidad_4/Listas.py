d421d60b7de2416117aaa6e5f771daac6f2eb6f9
sabrinagimenez98 <sabrinita9898@gmail.com>
2025-09-08 09:02:34 -0300

    Ordenando carpetas

---
+++ Unidad_4/Listas.py
@@ -0,0 +1,9 @@
+#1) Crear una lista con los números del 1 al 100 que sean múltiplos de 4. Utilizar la función
+#range.
+
+#2) Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el
+#penúltimo. ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el
+#indexing con números negativos!
+
+#3) Crear una lista vacía, agregar tres palabras con append e imprimir la lista resultante por
+#pantalla. Pista: para crear una lista vacía debes colocar los corchetes sin nada en su interior.
