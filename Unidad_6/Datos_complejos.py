79d067ac24ab24e1d7d95b9fb54ea460e4d5f2fc
sabrinagimenez98 <sabrinita9898@gmail.com>
2025-10-06 08:39:09 -0300

    Create Datos_complejos.py

---
+++ Unidad_6/Datos_complejos.py
@@ -0,0 +1,15 @@
+#1) Dado el diccionario precios_frutas 
+# precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva':1450}
+# Añadir las siguientes frutas con sus respectivos precios:
+# ● Naranja = 1200 ● Manzana = 1500 ● Pera = 2300
+
+#2) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código
+# desarrollado en el punto anterior, actualizar los precios de las siguientes frutas:
+# ● Banana = 1330  ● Manzana = 1700 ● Melón = 2800
+
+#3) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código desarrollado 
+# en el punto anterior, crear una lista que contenga únicamente las frutas sin los precios.
+
+#4) Escribí un programa que permita almacenar y consultar números telefónicos.
+#• Permití al usuario cargar 5 contactos con su nombre como clave y número como valor.
+#• Luego, pedí un nombre y mostrale el número asociado, si existe.
