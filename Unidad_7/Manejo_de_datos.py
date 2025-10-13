e062742b47e517f419fe463e2951d0d3d6ddb618
sabrinagimenez98 <sabrinita9898@gmail.com>
2025-10-13 08:15:35 -0300

    Create Manejo_de_datos.py

---
+++ Unidad_7/Manejo_de_datos.py
@@ -0,0 +1,22 @@
+#1. Crear archivo inicial con productos: Crear un archivo de texto llamado
+# productos.txt con tres productos. Cada línea debe tener: nombre,precio,cantidad
+
+# 2. Leer y mostrar productos: Crear un programa que abra productos.txt, lea cada
+# línea, la procese con .strip() y .split(","), y muestre los productos en el siguiente formato:
+# Producto: Lapicera | Precio: $120.5 | Cantidad: 30
+
+# 3. Agregar productos desde teclado: Modificar el programa para que luego de mostrar
+# los productos, le pida al usuario que ingrese un nuevo producto (nombre, precio,
+# cantidad) y lo agregue al archivo sin borrar el contenido existente.
+
+# 4. Cargar productos en una lista de diccionarios: Al leer el archivo, cargar los datos en
+# una lista llamada productos, donde cada elemento sea un diccionario con claves:
+# nombre, precio, cantidad.
+
+# 5. Buscar producto por nombre: Pedir al usuario que ingrese el nombre de un
+# producto. Recorrer la lista de productos y, si lo encuentra, mostrar todos sus datos. Si
+# no existe, mostrar un mensaje de error.
+
+# 6. Guardar los productos actualizados: Después de haber leído, buscado o agregado
+# productos, sobrescribir el archivo productos.txt escribiendo nuevamente todos los
+# productos actualizados desde la lista.
